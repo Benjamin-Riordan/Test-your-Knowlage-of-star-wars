0d74cfc65e10af041209dcd943ddd26bd8beeda9
Benjamin-Riordan <>
2023-05-16 09:34:35 +0000

    adding in js questions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,5 @@
-console.log("hello World")
+function getUserName() {
+    var username = document.getElementById("username").value;
+    var welcomeMessage = "Welcome " + username + "! How well do you know Star Wars? Let's find out.";
+    document.getElementById("username").innerHTML = welcomeMessage;
+}
