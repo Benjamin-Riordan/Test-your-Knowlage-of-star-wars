0f47ec8e68129f072bd34f0f53fe20635d98f3e4
Benjamin-Riordan <>
2023-05-16 11:44:18 +0000

    removed all code exepct for function to get working

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,61 +1,28 @@
-let username = document.getElementById("username")
-let questions = {
-    level : 1
-    {
-        questions: [
-              {
-                question: "What is the name of the protagonist in the original Star Wars trilogy?",
-                choices: ["Luke Skywalker", "Han Solo", "Darth Vader", "Princess Leia"],
-                correctAnswer: 0
-              },
-              {
-                question: "Which planet is home to Chewbacca, the Wookiee?",
-                choices: ["Hoth", "Tatooine", "Endor", "Kashyyyk"],
-                correctAnswer: 3
-              },
-              {
-                question: "Who is the master Jedi that trained Obi-Wan Kenobi?",
-                choices: ["Qui-Gon Jinn", "Yoda", "Mace Windu", "Count Dooku"],
-                correctAnswer: 0
-              },
-              {
-                question: "What is the primary weapon used by Jedi Knights?",
-                choices: ["Lightsaber", "Blaster", "Bowcaster", "Vibroblade"],
-                correctAnswer: 0
-              },
-              {
-                question: "Which famous smuggler is the captain of the Millennium Falcon?",
-                choices: ["Lando Calrissian", "Poe Dameron", "Jabba the Hutt", "Han Solo"],
-                correctAnswer: 3
-              },
-              {
-                question: "Who is Darth Vader's Sith apprentice in 'Star Wars: Episode III - Revenge of the Sith'?",
-                choices: ["Darth Maul", "Darth Sidious", "Darth Tyranus", "Darth Vader himself"],
-                correctAnswer: 2
-              },
-              {
-                question: "What is the name of the desert planet where Anakin Skywalker is discovered in 'Star Wars: Episode I - The Phantom Menace'?",
-                choices: ["Tatooine", "Coruscant", "Naboo", "Mustafar"],
-                correctAnswer: 0
-              },
-              {
-                question: "Which bounty hunter captures Han Solo in 'Star Wars: Episode V - The Empire Strikes Back'?",
-                choices: ["Boba Fett", "Jango Fett", "IG-88", "Dengar"],
-                correctAnswer: 0
-              },
-              {
-                question: "Who destroys the second Death Star in 'Star Wars: Episode VI - Return of the Jedi'?",
-                choices: ["Luke Skywalker", "Darth Vader", "Han Solo", "Lando Calrissian"],
-                correctAnswer: 3
-              },
-              {
-                question: "What species is Yoda?",
-                choices: ["Human", "Wookiee", "Ewok", "Yoda's species is unknown"],
-                correctAnswer: 3
-              },
-    }
-
+function welcomeUser() {
+    let name = document.getElementById("username").value;
+    let welcomeMessage =`<p>   
+     Hello ${name} ! Let's test your knowledge of Star Wars.
+     </p>`
+    document.getElementById("username-box").innerHTML = welcomeMessage;
 }
+
+document.getElementById("username").addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        welcomeUser();
+    }
+});
+
+function playgame()
+
+function keepscore()
+
+
+
+   
+
+
+    
+
 
 
 
