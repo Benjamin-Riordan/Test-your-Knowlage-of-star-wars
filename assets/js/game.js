7095074b8d67f4d0853f028d15be2f11e188f0d7
Benjamin-Riordan <>
2023-05-18 17:55:50 +0000

    added in a user name check to see if user had inputted name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,18 @@
 function welcomeUser() {
-    let name = document.getElementById("username").value;
+    const name = document.getElementById("username").value;
+    if(name === ""){
+        alert("please enter a username")
+        return;
+    }
     let welcomeMessage =`<p>   
      Hello ${name} ! Let's test your knowledge of Star Wars.
-     </p>`
+     </p>`;
     document.getElementById("username-box").innerHTML = welcomeMessage;
 }
 document.getElementById("username").addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
         welcomeUser();
-    }
+    } 
 });
 
 document.addEventListener("DOMContentLoaded", function() {
